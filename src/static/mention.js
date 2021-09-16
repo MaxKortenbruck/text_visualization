f93dcbd76ec561dcb796248f956e179e050d1dbf
Max Kortenbruck <max.kortenbruck@uni-wuppertal.de>
2021-09-16 22:44:12 +0200

    mention documentation done

--- src/static/mention.js
+++ src/static/mention.js
@@ -1,6 +1,22 @@
 'use strict'
 
+/**
+ * The Mention class. You can create a new Object by calling the constructur as:
+ * >>> mention = new Mnetion(sentence, text, tokens, annotation_type, head_token_index,
+ *              head-Token_word, index ,political_direction)
+ */
 export class Mention {
+    /**
+     * 
+     * @param {Integer} sent - Number of the sentence 
+     * @param {String} txt - Text of the mention
+     * @param {Array} tok - The tokens of the mention 
+     * @param {String} annot - The annotation type 
+     * @param {Integer} hti - The head token index
+     * @param {String} htw - The head token word
+     * @param {Integer} ind - The index of the entity, incrementaly based on the JSON data structure
+     * @param {String} pol - Political direction of the mention's articel
+     */
     constructor(sent, txt, tok, annot, hti, htw, ind, pol )
     {
         this._sentence = sent;
@@ -11,68 +27,45 @@ export class Mention {
         this._head_token_index = hti;
         this._head_token_word = htw;
         this._political_direction_of_article = pol;
-        
-        // let temp = ent.merging_history.original.phrases[ind][1].split("_");
-        // console.log("temp: " + temp)
-        // console.log(ent.merging_history.original.phrases[ind][0])
-        // console.log(this._text)
-        // if(this._text === ent.merging_history.original.phrases[ind][0])
-        // {
-        //     let ph = temp[1].split("_");
-        //     this._political_direction_of_article = ph[1];
-        // }
-        // else {console.log("Error in phrases and text index")}
-
-        //this._normalized_text = this.set_norm_text(this._text);
-        // this._political_direction_of_article = temp[1];
-        // console.log(this._political_direction_of_article);
-
     }
-
+    /**
+     * @returns The political direction of the article the mentioon belongs to
+     */
     get political_direction_of_article()
     {
         return this._political_direction_of_article;
     }
-    
+    /**
+     * @returns The text of the mention
+     */    
     get text()
     {
         return this._text;
     }
-    
+    /**
+     * @returns An Array of tokens, the indexes of the mention's words in the respective sentence
+     */    
     get tokens()
     {
         return this._tokens;
     }
-
+    /**
+     * @returns The annotation type of the mention
+     */
     get annotate_type()
     {
         return this._annotate_type;
     }
-
+    /**
+     * @returns The mentions index, incrementally based on the JSON datastructure
+     */
     get index()
     {
         return this._index;
     }
-
-    get normalized_text()
-    {
-        let lower = this.text.toLowerCase();
-        //console.log("normalize");
-        //console.log(lower);
-        if(lower[0] == "the"){lower.shift()}
-
-        let splt = lower.split(" ")
-        for(let i = 0; i < splt.length; i++)
-        {
-            splt[i] = splt[i].replace(/[´,`,;,.,,,",:,!.?,(,)]/g, "");
-        }
-        //console.log(splt);
-        if(splt[0] == "the")
-        {splt.shift()};
-
-        return splt.join(" ");
-    }
-
+    /**
+     * @returns The index of the sentence the mention is located
+     */
     get sentence()
     {
         return this._sentence;
